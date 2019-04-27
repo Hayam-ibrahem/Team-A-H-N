e390665be62e1ce927fdd496ded58aac4c808160
Nooranshibl <34580774+Nooranshibl@users.noreply.github.com>
2019-04-27 23:14:17 +0200

    N

--- urls.py
+++ urls.py
@@ -9,9 +9,6 @@ urlpatterns = [
     path('register_company', views.register_company, name='register_company'),
     path('all_users', views.get_users, name='get_users'),
     path('user_info/<user_ID>', views.userByID, name='userByID'),
-<<<<<<< HEAD
     path('all_companys', views.get_companys, name='get_companys'),
-=======
-      path('company_info/<company_ID>', views.companyByID, name='companyByID')
->>>>>>> d4a0dd3b2727a2f1f885873e59d652f10173cc54
+    path('company_info/<company_ID>', views.companyByID, name='companyByID')
 ]
--- views.py
+++ views.py
@@ -89,26 +89,25 @@ def userByID(request,user_ID):
         return HttpResponse ("User not found")
 
     return HttpResponse("User not found")
-<<<<<<< HEAD
+
 def get_companys(request):
     try:
         company = Company.objects.all()
-=======
-
-    def companyByID(request,company_ID):
-    try:
-        company = Company.objects.get(pk=company_ID)
->>>>>>> d4a0dd3b2727a2f1f885873e59d652f10173cc54
         data = serializer.serialize('json', company)
         return HttpResponse(data, content_type='application.json')
 
     except Exception:
-<<<<<<< HEAD
         return HttpResponse ("No companys")
 
     return HttpResponse("No companys")
-=======
+
+def companyByID(request,company_ID):
+    try:
+        company = Company.objects.get(pk=company_ID)
+        data = serializer.serialize('json', company)
+        return HttpResponse(data, content_type='application.json')
+
+    except Exception:
         return HttpResponse ("Company not found")
 
     return HttpResponse("Company not found")
->>>>>>> d4a0dd3b2727a2f1f885873e59d652f10173cc54
