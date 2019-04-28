a54a23a6c9dd92472ca6824b6fdcf1fd06094e82
Nooranshibl <34580774+Nooranshibl@users.noreply.github.com>
2019-04-29 00:57:39 +0200

    improve unit tests coverage

--- users/tests.py
+++ users/tests.py
@@ -3,6 +3,7 @@ from django.test import TestCase
 # Create your tests here.
 from django.test import TestCase
 from users.models import User
+from users.models import Company
 
 class UserTestCase(TestCase):
     def setUp(self):
@@ -30,29 +31,34 @@ class UserTestCase(TestCase):
         self.assertEqual(user.gender, "Female")
         self.assertEqual(user.interests, "Interests")
 
+    def test_login_validation(self):
+        response = self.client.get('/login_validation/TestUser/TestPassword')
+        self.assertIsNotNone(response)
+        self.assertEqual(response.status_code, 200)
+        response_content = response.content.decode('utf-8')
+        self.assertEqual(response_content, '{"user_ID": 1}')
+
 
 class CompanyTestCase(TestCase):
     def setUp(self):
-        User.objects.create(
-            user_name="TestUser",
-            password="TestPassword",
-            first_name="TestFirstName",
-            last_name="TestLastName",
+        Company.objects.create(
+            company_name="TestCompanyName",
+            company_password="TestPassword",
+            name="TestName",
+            location="TestLocation",
+            nOfEmployees="200",
             email="Test@email.com",
-            age="23",
-            gender="Female",
-            interests="Interests")
+            company_interests="Interests")
 
-    def test_user_is_in_database(self):
-        self.assertIsNotNone(User.objects.get(user_name="TestUser"))
+    def test_company_is_in_database(self):
+        self.assertIsNotNone(Company.objects.get(company_name="TestCompanyName"))
 
-    def test_user_fields(self):
-        user = User.objects.get(user_name="TestUser")
-        self.assertEqual(user.user_name, "TestUser")
-        self.assertEqual(user.password, "TestPassword")
-        self.assertEqual(user.first_name, "TestFirstName")
-        self.assertEqual(user.last_name, "TestLastName")
-        self.assertEqual(user.email, "Test@email.com")
-        self.assertEqual(user.age, "23")
-        self.assertEqual(user.gender, "Female")
-        self.assertEqual(user.interests, "Interests")
+    def test_company_fields(self):
+        company = Company.objects.get(company_name="TestCompanyName")
+        self.assertEqual(company.company_name, "TestCompanyName")
+        self.assertEqual(company.company_password, "TestPassword")
+        self.assertEqual(company.name, "TestName")
+        self.assertEqual(company.location, "TestLocation")
+        self.assertEqual(company.nOfEmployees, "200")
+        self.assertEqual(company.email, "Test@email.com")
+        self.assertEqual(company.company_interests, "Interests")
